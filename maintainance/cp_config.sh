cecff5b0c2ef52dcea862e802a4bd41a044c4f0b
Connor Li <connorli0@outlook.com>
2024-01-28 18:43:54 -0500

    update

--- maintainance/cp_config.sh
+++ maintainance/cp_config.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# copy config file as user steam
+sudo -u steam -s
 cp /home/cosc_3p97/palworld-terraform/maintainance/PalWorldSettings.ini /home/steam/.steam/SteamApps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
